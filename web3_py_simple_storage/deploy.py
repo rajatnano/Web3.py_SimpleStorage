fcdc90fa98ee97ec88be769374eb36c1c6381f15
Rajat <98547181+rajatnano@users.noreply.github.com>
2022-02-04 22:09:10 +0530

    Update deploy.py
    reading solidity smart contract

--- web3_py_simple_storage/deploy.py
+++ web3_py_simple_storage/deploy.py
@@ -1,4 +1,30 @@
-#Read smart contract
-with open("./SimpleStorage.sol") as file:
-  simple_storage_file = file.read()
-  print(simple_storage_file)
+# Python src C:\Users\rajat\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Python.3.9_qbz5n2kfra8p0\python.exe -m pip install --upgrade pip
+
+# Solidity Compiler
+from solcx import compile_standard, install_solc
+
+# Read smart contract
+with open("./SimpleStorage.sol", "r") as file:
+    simple_storage_file = file.read()
+
+# Install solc
+print("Installing...")
+install_solc("0.6.0")
+
+# Solidity source code
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {
+                    "*": ["abi", "metadata", "evm.bytecode", "evm.bytecode.sourceMap"]
+                }
+            }
+        },
+    },
+    solc_version="0.6.0",
+)
+
+print(compiled_sol)
